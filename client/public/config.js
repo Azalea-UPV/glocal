d716ad1a18867920ce3a1c25074d77cf1cbc7075
Nicolas Garcia Sastre <n.garcia.sastre@gmail.com>
2024-03-31 19:47:44 +0200

    config

--- client/public/config.js
+++ client/public/config.js
@@ -1,6 +1,6 @@
 const env = {
     APP_NAME: "Azalea Participación",
     APP_ICON_URL: "/azalea-icon.png",
-    SERVER_URL: "http://localhost:5000",
+    SERVER_URL: "https://report.azaleaupv.com:5050",
     TILES_URL: "/tiles/{z}/{x}/{y}.png"
 }
